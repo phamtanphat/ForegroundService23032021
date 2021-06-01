c34c82eb2c843ab46322d597ccfe0b51de58cb73
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-01 20:39:27 +0700

    create notification

--- app/src/main/java/com/example/foregroundservice23032021/MyService.java
+++ app/src/main/java/com/example/foregroundservice23032021/MyService.java
@@ -1,15 +1,21 @@
 package com.example.foregroundservice23032021;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
-//    alt + enter
+    //    alt + enter
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -19,7 +25,7 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("BBB","onCreate Service");
+        Log.d("BBB", "onCreate Service");
     }
 
     @Override
@@ -31,6 +37,23 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d("BBB","onDestroy Service");
+        Log.d("BBB", "onDestroy Service");
+    }
+
+    public Notification createNotification(Context context, String channelId) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
+        builder.setSmallIcon(R.drawable.ic_baseline_message_24);
+        builder.setContentTitle("Hệ thống có thông báo mới");
+        builder.setContentText("Có tin nhắn mới");
+        builder.setShowWhen(true);
+        builder.setPriority(Notification.PRIORITY_HIGH);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channelId, "CHANNEL_NAME", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        return builder.build();
     }
 }
