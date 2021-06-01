506ab135908679ab5f37c31d617ade5ce1ce7b03
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-06-01 21:04:33 +0700

    finish

--- app/src/main/java/com/example/foregroundservice23032021/MyService.java
+++ app/src/main/java/com/example/foregroundservice23032021/MyService.java
@@ -7,6 +7,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,7 +16,10 @@ import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
-    //    alt + enter
+
+    String CHANNEL_ID = "CHANNEL_ID";
+    int mCount = 1;
+    NotificationManager mNotificationManager;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -26,11 +30,19 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("BBB", "onCreate Service");
+        startForeground(1 , createNotification(this,mCount,CHANNEL_ID));
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "onStartCommand Service", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mNotificationManager.notify(1,createNotification(MyService.this,++mCount,CHANNEL_ID));
+                new Handler().postDelayed(this,1000);
+            }
+        },1000);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -40,19 +52,19 @@ public class MyService extends Service {
         Log.d("BBB", "onDestroy Service");
     }
 
-    public Notification createNotification(Context context, String channelId) {
+    public Notification createNotification(Context context, int count, String channelId) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId);
         builder.setSmallIcon(R.drawable.ic_baseline_message_24);
         builder.setContentTitle("Hệ thống có thông báo mới");
-        builder.setContentText("Có tin nhắn mới");
+        builder.setContentText("Có " +count + " tin nhắn mới" );
         builder.setShowWhen(true);
         builder.setPriority(Notification.PRIORITY_HIGH);
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel(channelId, "CHANNEL_NAME", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(notificationChannel);
+            mNotificationManager.createNotificationChannel(notificationChannel);
         }
         return builder.build();
     }
